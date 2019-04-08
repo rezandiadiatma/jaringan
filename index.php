6676f47f1687f1b5603e7b774fa67f20d4b02dcd
rezandiadiatma <41320246+rezandiadiatma@users.noreply.github.com>
2019-04-08 08:40:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="style.css" rel="stylesheet">
+<script src="bootstrap.min.js"></script>
+<script src="jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-3 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="udinus_logo.png">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a href="#">Rezandi Adiatma</a>
+                    </div>
+                    <div class="desc">A22.2017.02584</div>
+                    <div class="desc">D3 Teknik Informatika</div>
+                    <div class="desc">Universitas Dian Nuswantoro</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/rezandi_a">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" rel="publisher"
+                       href="https://plus.google.com/rezandiadiatma">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" rel="publisher"
+                       href="https://facebook.com/dodookie">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+	</div>
+</div>
